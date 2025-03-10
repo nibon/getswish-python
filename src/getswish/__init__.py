71a35d2e7b14577bf1d8187961041e19b37cbc7d
Daniel Nibon <daniel@nibon.se>
2025-03-10 09:48:27 +0100

    Bump to version 0.3.0.

--- src/getswish/__init__.py
+++ src/getswish/__init__.py
@@ -5,4 +5,4 @@ from .environments import Certificate, Certificates, Environment, ProductionEnvi
 from .exceptions import SwishError  # noqa F401
 from .models import Payment, Payout, Refund  # noqa F401
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
